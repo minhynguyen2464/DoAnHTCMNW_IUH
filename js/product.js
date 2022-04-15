2ecc528be43e8ed9690d77f691c2e66cad27d568
minhynguyen2464 <minhynguyen97@gmail.com>
2022-04-15 15:19:24 +0700

    Combine all product to 1 folder & fixing some bug

--- js/product.js
+++ js/product.js
@@ -1,46 +1,46 @@
 
 $(document).ready(function () {
     $("#of27-btn1").click(function () { 
-        document.getElementById("of27-pic").src="../picture/of27-1.webp"
+        document.getElementById("of27-pic").src="../../picture/of27-1.webp"
     });
     
     $("#of27-btn2").click(function () { 
-        document.getElementById("of27-pic").src="../picture/of27-2.webp"
+        document.getElementById("of27-pic").src="../../picture/of27-2.webp"
     });    
-
+    
     $("#of02-btn1").click(function () { 
-        document.getElementById("of02-pic").src="../picture/product/of02-1.webp"
+        document.getElementById("of02-pic").src="../../picture/product/of02-1.webp"
     });    
 
     $("#of02-btn2").click(function () { 
-        document.getElementById("of02-pic").src="../picture/product/of02-2.webp"
+        document.getElementById("of02-pic").src="../../picture/product/of02-2.webp"
     });    
 
     $("#of02-btn3").click(function () { 
-        document.getElementById("of02-pic").src="../picture/product/of02-3.webp"
+        document.getElementById("of02-pic").src="../../../picture/product/of02-3.webp"
     });    
 
     $("#of07-btn1").click(function () { 
-        document.getElementById("of07-pic").src="../picture/product/of07-1.webp"
+        document.getElementById("of07-pic").src="../../picture/product/of07-1.webp"
     });    
 
     $("#of07-btn2").click(function () { 
-        document.getElementById("of07-pic").src="../picture/product/of07-2.webp"
+        document.getElementById("of07-pic").src="../../picture/product/of07-2.webp"
     });    
 
     $("#of07-btn3").click(function () { 
-        document.getElementById("of07-pic").src="../picture/product/of07-3.webp"
+        document.getElementById("of07-pic").src="../../picture/product/of07-3.webp"
     });    
 
     $("#of18-btn1").click(function () { 
-        document.getElementById("of18-pic").src="../picture/product/of18-1.webp"
+        document.getElementById("of18-pic").src="../../picture/product/of18-1.webp"
     });    
 
     $("#of18-btn2").click(function () { 
-        document.getElementById("of18-pic").src="../picture/product/of18-2.webp"
+        document.getElementById("of18-pic").src="../../picture/product/of18-2.webp"
     });    
 
     $("#of18-btn3").click(function () { 
-        document.getElementById("of18-pic").src="../picture/product/of18-3.webp"
+        document.getElementById("of18-pic").src="../../picture/product/of18-3.webp"
     });    
 });
